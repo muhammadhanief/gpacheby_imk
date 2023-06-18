b36de96e16ce2f53ac985ffc57aa8de2236be96b
muhammadhanief <haniefm19@gmail.com>
2023-06-18 11:29:35 +0700

    biar diclik work ga muncul

--- components/header.tsx
+++ components/header.tsx
@@ -6,10 +6,84 @@ import { FC, useState } from "react";
 import { ChevronDownIcon, Bars3Icon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 import { slide as Menu } from "react-burger-menu";
-import Button from "./button";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // batas yang dipindah
+  interface NavItemProps {
+    name: string;
+    href: string;
+    children?: NavItemProps[];
+  }
+
+  const NavItem: FC<NavItemProps> = ({ name, href, children }) => {
+    const pathName = usePathname();
+    console.log(pathName);
+    console.log(href);
+    console.log(pathName.split("/").at(0) === href.split("/").at(0));
+    console.log(pathName.split("/"));
+
+    if (children) {
+      return (
+        <div className="group">
+          <div
+            className={`hover:bg-cheby text-white group px-4 py-2 rounded transition-colors duration-300 no-underline flex gap-2 items-center hover:text-black`}
+          >
+            {name}
+            <ChevronDownIcon className="h-4 w-4"></ChevronDownIcon>
+          </div>
+          {pathName.split("/").at(1) === href.split("/").at(1) &&
+          href.split("/").at(1) !== "" ? (
+            <div className="w-full h-1 -mt-0.5 bg-cheby"></div>
+          ) : null}
+          <div className="flex-col hidden group-hover:flex absolute bg-black rounded">
+            {children.map((child) => (
+              <Link
+                onClick={() => setIsMenuOpen(false)}
+                className="hover:no-underline hover:bg-cheby group px-4 py-2 rounded transition-colors duration-300 no-underline text-white hover:text-black "
+                href={child.href}
+                key={child.name}
+              >
+                {child.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      );
+    }
+    return (
+      <Link
+        onClick={() => setIsMenuOpen(false)}
+        href={href}
+        className="hover:bg-cheby group px-4 py-2 rounded transition-colors duration-300 no-underline hover:no-underline"
+      >
+        <div className="text-white  group-hover:text-black ">{name}</div>
+        {pathName === href ? (
+          <div className="w-full h-1 mt-1.5 bg-cheby"></div>
+        ) : null}
+      </Link>
+    );
+  };
+
+  const NAVIGATION = [
+    { name: "Beranda", href: "/" },
+    { name: "Tentang Kami", href: "/tentang_kami" },
+    { name: "Sejarah", href: "/sejarah" },
+    {
+      name: "Artikel",
+      href: "/artikel",
+      children: [
+        { name: "Kegiatan", href: "/artikel/kegiatan" },
+        { name: "Peringatan Hari", href: "/artikel/hari_penting" },
+      ],
+    },
+    { name: "Pendakian Umum", href: "/pendakian_umum" },
+    { name: "Catatan Perjalanan", href: "/catatan_perjalanan" },
+  ];
+
+  // batas bawah yang dipindah
+
   return (
     <header className="z-50 relative bg-black">
       <nav className="flex w-full justify-between overflow-hidden items-center ">
@@ -48,74 +122,5 @@ const Header = () => {
     </header>
   );
 };
-
-interface NavItemProps {
-  name: string;
-  href: string;
-  children?: NavItemProps[];
-}
-
-const NavItem: FC<NavItemProps> = ({ name, href, children }) => {
-  const pathName = usePathname();
-  console.log(pathName);
-  console.log(href);
-  console.log(pathName.split("/").at(0) === href.split("/").at(0));
-  console.log(pathName.split("/"));
-
-  if (children) {
-    return (
-      <div className="group">
-        <div
-          className={`hover:bg-cheby text-white group px-4 py-2 rounded transition-colors duration-300 no-underline flex gap-2 items-center hover:text-black`}
-        >
-          {name}
-          <ChevronDownIcon className="h-4 w-4"></ChevronDownIcon>
-        </div>
-        {pathName.split("/").at(1) === href.split("/").at(1) &&
-        href.split("/").at(1) !== "" ? (
-          <div className="w-full h-1 -mt-0.5 bg-cheby"></div>
-        ) : null}
-        <div className="flex-col hidden group-hover:flex absolute bg-black rounded">
-          {children.map((child) => (
-            <Link
-              className="hover:no-underline hover:bg-cheby group px-4 py-2 rounded transition-colors duration-300 no-underline text-white hover:text-black "
-              href={child.href}
-              key={child.name}
-            >
-              {child.name}
-            </Link>
-          ))}
-        </div>
-      </div>
-    );
-  }
-  return (
-    <Link
-      href={href}
-      className="hover:bg-cheby group px-4 py-2 rounded transition-colors duration-300 no-underline hover:no-underline"
-    >
-      <div className="text-white  group-hover:text-black ">{name}</div>
-      {pathName === href ? (
-        <div className="w-full h-1 mt-1.5 bg-cheby"></div>
-      ) : null}
-    </Link>
-  );
-};
-
-const NAVIGATION = [
-  { name: "Beranda", href: "/" },
-  { name: "Tentang Kami", href: "/tentang_kami" },
-  { name: "Sejarah", href: "/sejarah" },
-  {
-    name: "Artikel",
-    href: "/artikel",
-    children: [
-      { name: "Kegiatan", href: "/artikel/kegiatan" },
-      { name: "Peringatan Hari", href: "/artikel/hari_penting" },
-    ],
-  },
-  { name: "Pendakian Umum", href: "/pendakian_umum" },
-  { name: "Catatan Perjalanan", href: "/catatan_perjalanan" },
-];
 
 export default Header;
